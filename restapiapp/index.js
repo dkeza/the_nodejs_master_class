ac0a1d0045c8dc08448bf14f434731deddc218f1
keza <dk@citadel.rs>
2018-08-22 22:04:33 +0200

    Simple server

--- restapiapp/index.js
+++ restapiapp/index.js
@@ -1,1 +1,9 @@
-console.log('Started...');
+let http = require('http');
+
+let server = http.createServer(function(req, res) {
+    res.end('Hello World\n');
+});
+
+server.listen(3000, function() {
+    console.log('Listening on 3000');
+})
