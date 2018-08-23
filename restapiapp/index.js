96fe3172be778ccbb1e851da598765ac7c11240c
keza <dk@citadel.rs>
2018-08-23 20:59:21 +0200

    Parse path

--- restapiapp/index.js
+++ restapiapp/index.js
@@ -1,7 +1,15 @@
 let http = require('http');
+let url = require('url');
 
 let server = http.createServer(function(req, res) {
+    let parsedUrl = url.parse(req.url, true);
+
+    let path = parsedUrl.pathname;
+    let trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
     res.end('Hello World\n');
+
+    console.log(trimmedPath);
 });
 
 server.listen(3000, function() {
